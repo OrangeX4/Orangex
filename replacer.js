f28c67c1ba9350455b2a305e6e1e56aade08c9fe
OrangeX4 <318483724@qq.com>
2020-02-02 23:15:39 +0800

    sd

--- replacer.js
+++ replacer.js
@@ -1,11 +1,11 @@
 // Test:
 var str = "jsx js ddd  ddd 中文 中Eng 1数字-Head $";
-var dict = '{"jsx":"爪纹","ddd":"顶顶顶"}';
+var dict = {"jsx":"爪纹","ddd":"顶顶顶"};
 console.log(replaceContent(str,dict));
 
 
-function replaceContent(content, dictionary) {
-    dict = JSON.parse(dictionary);
+function replaceContent(content, dict) {
+    // var dict = JSON.parse(dictionary);
     var patt = /[\u4E00-\u9FA5A-Za-z0-9_$-]+/g;
     // var match = unique(str.match(patt));  // 去重版本
     var match = content.match(patt);
