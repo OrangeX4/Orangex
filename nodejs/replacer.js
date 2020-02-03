4210f99ba7e69525059938a7d003e8bc321cb251
OrangeX4 <318483724@qq.com>
2020-02-03 14:17:05 +0800

    Add file function

--- nodejs/replacer.js
+++ nodejs/replacer.js
@@ -4,25 +4,49 @@
 // console.log(replaceContent(str,dict));
 
 //module.exports ={replaceContent:replaceContent};
-
+var split = "￥";
+exports.setSplit = function(splitVar){
+    split = splitVar;
+};
 exports.replaceContent = replaceContent;
+exports.replaceWithSplit = replaceWithSplit;
 function replaceContent(content, dict) {
     // var dict = JSON.parse(dictionary);
     var patt = /[\u4E00-\u9FA5A-Za-z0-9_$-]+/g;
     // var match = unique(str.match(patt));  // 去重版本
     var match = content.match(patt);
-    var miss = [];
+    var fail = [];
+    var success = [];
     console.log(match);
     for (var i in match) {
         // console.log(match[i]);n
         if (dict[match[i]]) {
             content = content.replace(match[i], dict[match[i]]);
+            success.push(match[i]);
         } else {
-            miss.push(match[i]);
+            fail.push(match[i]);
         }
         // console.log(str);
     }
-    return {content:content,miss:miss};
+    return {content:content,success:success,fail:fail};
+}
+
+function replaceWithSplit(content,dict){
+    var strArray = content.split(split);
+    var str = "";
+    var objectArray = [];
+    var returnValue = {};
+    for(i in strArray){
+        if(i % 2 == 0){
+            returnValue = replaceContent(strArray[i],dict);
+            str = str + returnValue.content;
+            objectArray.push(returnValue);
+        }else{
+            str = str + strArray[i];
+            objectArray.push(strArray[i]);
+        }
+    }
+    return {content:str,returnArray:objectArray};
 }
 
 // function unique(arr) {
--- nodejs/test.js
+++ nodejs/test.js
@@ -1,5 +1,34 @@
 var replacer = require('./replacer');
 
-var str = "jsx js ddd  ddd 中文 中Eng 1数字-Head $";
-var dict = {"jsx":"爪纹","ddd":"顶顶顶"};
-console.log(replacer.replaceContent(str,dict));
+function test(dict) {
+    var str = "jsx js if  ddd  ddd ￥中文 中Eng￥ 1数字-Head $";
+    // var dict = {"jsx":"爪纹","ddd":"顶顶顶"};
+    console.log(replacer.replaceWithSplit(str, dict));
+}
+
+var fs = require("fs");
+var data = '';
+
+// 创建可读流
+var readerStream = fs.createReadStream('./map/dict001.json');
+
+// 设置编码为 utf8。
+readerStream.setEncoding('UTF8');
+
+// 处理流事件 --> data, end, and error
+readerStream.on('data', function (chunk) {
+    data += chunk;
+});
+
+readerStream.on('end', function () {
+    // console.log(data);
+
+    dictionary = JSON.parse(data);
+    test(dictionary.common);
+});
+
+readerStream.on('error', function (err) {
+    console.log(err.stack);
+});
+
+console.log("程序执行完毕");
