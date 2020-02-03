d2837b0576cc5467598c7e9127760301a64f7249
OrangeX4 <318483724@qq.com>
2020-02-03 23:38:26 +0800

    Add download function

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,6 +1,26 @@
 var replacer = require('./replacer');
 var utils = require('./utils');
 var str = "jsx js if  ddd  ddd ￥中文 中Eng￥ 1数字-Head $";
+// var str = `function replaceContent(content, dict) {
+//     // var dict = JSON.parse(dictionary);
+//     var patt = /[\u4E00-\u9FA5A-Za-z0-9_$-]+/g;
+//     // var match = unique(str.match(patt));  // 去重版本
+//     var match = content.match(patt);
+//     var fail = [];
+//     var success = [];
+//     // console.log(match);
+//     for (var i in match) {
+//         // console.log(match[i]);n
+//         if (dict[match[i]]) {
+//             content = content.replace(match[i], dict[match[i]]);
+//             success.push(match[i]);
+//         } else {
+//             fail.push(match[i]);
+//         }
+//         // console.log(str);
+//     }
+//     return {content:content,success:success,fail:fail};
+// }`;
 utils.readWithWebAndRedirect(testFunc);
 
 function testFunc(dict) {
@@ -10,66 +30,9 @@ function testFunc(dict) {
     var returnObject = replacer.replaceWithSplit(str, JSON.parse(dict).common);
     console.log("All:");
     console.log(returnObject);
-    console.log("Array content:");
-    console.log(returnObject.returnArray[0]);
+    // console.log("Array content:");
+    // console.log(returnObject.returnArray[0]);
+    console.log("Content:");
+    console.log(returnObject.content);
 
-}
-function readWithHttpsAndRedirect(callback,url){
-    readWithHttps(function(html){
-        var strArray = html.match(/(?<=").*(?=")/);   // (?<=Head：).*(?=Tail)
-        if(strArray.length != 0){
-            console.log(strArray[0]);
-            readWithHttps(callback,strArray[0]);
-        }
-    });
-}
-function readWithHttps(callback,url) {
-
-    if (!url) {
-        url = "https://github.com/OrangeX4/Orangex/releases/download/0.0.2/dict.json";
-    }
-    const http = require('https');
-
-    http.get(url, function (req, res) {
-        let html = '';
-        req.on('data', function (data) {
-            html += data;
-        });
-        req.on('end', function () {
-            // let result = JSON.parse(html);
-            // callback(JSON.parse(html));
-            callback(html);
-        });
-    });
-}
-
-
-function readWithFile(callback, url) {
-    var fs = require("fs");
-    var data = '';
-
-    if (!url) {
-        url = "./map/dict.json";
-    }
-    // 创建可读流
-    var readerStream = fs.createReadStream(url);
-
-    // 设置编码为 utf8。
-    readerStream.setEncoding('UTF8');
-
-    // 处理流事件 --> data, end, and error
-    readerStream.on('data', function (chunk) {
-        data += chunk;
-    });
-
-    readerStream.on('end', function () {
-        // console.log(data);
-        callback(data);
-    });
-
-    readerStream.on('error', function (err) {
-        console.log(err.stack);
-    });
-
-    // console.log("程序执行完毕");
 }
--- nodejs/utils.js
+++ nodejs/utils.js
@@ -3,7 +3,49 @@ var fs = require("fs");
 // readWithWebAndRedirect(str => console.log(str));
 exports.readWithWeb = readWithWeb;
 exports.readWithWebAndRedirect = readWithWebAndRedirect;
+exports.downloadWithWeb = downloadWithWeb;
+exports.downloadWithWebAndRedirect = downloadWithWebAndRedirect;
 exports.readWithFile = readWithFile;
+
+// downloadWithWebAndRedirect("./map/dict.json");
+
+function downloadWithWeb(path,url){
+    if (!url) {
+        url = "https://api.github.com/repos/Orangex4/Orangex/releases/latest";
+    }
+    if(!path){
+        path = "./map/dict.json";
+    }
+    fetch(url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/octet-stream' },
+    }).then(res => res.buffer()).then(_buffer => {
+        fs.writeFile(path, _buffer, "binary", function (err) {
+            console.log(err || path);
+        });
+        // callback(_buffer.toString("utf8"));
+    });
+
+}
+
+function downloadWithWebAndRedirect(path,url){
+    if (!url) {
+        url = "https://api.github.com/repos/Orangex4/Orangex/releases/latest";
+    }
+    readWithWeb(html => {
+        jsonObject = JSON.parse(html);
+        fileUrl = jsonObject.assets[0].browser_download_url;
+        // console.log(fileUrl);
+        downloadWithWeb(path,fileUrl);
+        // var strArray = html.match(/(?<=").*(?=")/);   // (?<=Head：).*(?=Tail)
+        // if(strArray.length != 0){
+            // console.log(strArray[0]);
+            // readWithHttps(callback,strArray[0]);
+            // readWithWeb(callback,strArray[0])
+        // }
+    },url);
+
+}
 
 function readWithWeb(callback,url){
     if (!url) {
