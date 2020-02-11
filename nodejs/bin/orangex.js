22c2d92e2f4c7207c881039059b22ca4c4b9937d
OrangeX4 <318483724@qq.com>
2020-02-11 12:47:54 +0800

    Fix updateDict

--- nodejs/bin/orangex.js
+++ nodejs/bin/orangex.js
@@ -4,7 +4,7 @@
  * @author <a href='https://github.com/OrangeX4/'>OrangeX4</a>
  * @version 0.1
  */
-const childProcess = require('child_process');
+// const childProcess = require('child_process');
 const Path = require('path');
 const fs = require('fs');
 const minimist = require('minimist');
@@ -17,11 +17,17 @@ console.log(argv);
 
 const index = require('../dist/main.js');
 
-function callback(err) {
-    if (err) console.log('读取最新字典失败!');
-    else console.log('读取最新字典成功!');
+function updateDict() {
+    console.log('正在更新字典文件, 请稍等一会:)');
+    index.readDictFileByGithubRelease('https://api.github.com/repos/Orangex4/Orangex/releases/latest').then((dict) => {
+        fs.writeFile(`${Path.dirname(argv._[1])}/dict.json`, dict, (err) => {
+            if (err) console.log('读取最新字典失败!');
+            else console.log('读取最新字典成功!');
+        });
+    });
 }
-// 判断字典文件是否存在, 不存在就执行命令"橙式 更新字典", 存在就进一步判断
+
+// 判断字典文件是否存在, 不存在就执行updateDict, 存在就进一步判断
 fs.exists(`${Path.dirname(argv._[1])}/dict.json`, (isExist) => {
     if (isExist) {
         if (argv._[2]) {
@@ -33,10 +39,7 @@ fs.exists(`${Path.dirname(argv._[1])}/dict.json`, (isExist) => {
                     index.test();
                     break;
                 case '更新字典':
-                    console.log('正在更新字典文件...');
-                    index.readDictFileByGithubRelease('https://api.github.com/repos/Orangex4/Orangex/releases/latest').then((dict) => {
-                    fs.writeFile(`${Path.dirname(argv._[1])}/dict.json`, dict, callback);
-                    });
+                    updateDict();
                     break;
                 case '英转汉':
                     index.translaterFileTree(Path.normalize(process.cwd()), `${Path.dirname(argv._[1])}/dict.json`, true, argv.深);
@@ -51,10 +54,7 @@ fs.exists(`${Path.dirname(argv._[1])}/dict.json`, (isExist) => {
                 console.log('使用"橙式 帮助"命令可获取帮助信息:)');
         }
     } else {
-        childProcess.exec('橙式 更新字典', (error) => {
-            if (error !== null) {
-                console.log(`运行错误！: ${error}`);
-            } else index.translaterFileTree(Path.normalize(process.cwd()), `${Path.dirname(argv._[1])}/dict.json`, true, argv.深);
-        });
+        // 更新字典
+        updateDict();
     }
 });
